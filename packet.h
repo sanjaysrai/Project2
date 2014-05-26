aa8990189ef2a0693a063ef2e0a12c3895f96cda
Sanjay <sanjay.s.rai@live.com>
2014-05-25 20:16:23 -0700

    Created the packet data structure in packet.h

---
+++ packet.h
@@ -0,0 +1,20 @@
+/*
+	This is the data structure we are going to use for 
+	our packet for our go-back-n implementation
+*/
+
+
+// What is the appropraite maximum size for the data?
+#define SIZE = 256
+
+struct packet
+{
+	// Want to use an enum for type instead of an int
+	int typeOfPacket;
+	// Data in packet
+	char data[SIZE];
+	// The amount of space the packet takes
+	int length;
+	// current packets sequence number
+	int sequence_number;
+};
