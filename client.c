3ce40a41f20e2c164d5f2c4d91f505bcafb3370c
Sanjay <sanjay.s.rai@live.com>
2014-05-26 21:24:43 -0700

    Filled in server information in data structure

--- client.c
+++ client.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <netinet/in.h> 
+#include <netdb.h>
 
 // auxillary function to print out errors and exit to save space
 void error(char* msg)
@@ -12,23 +14,39 @@ void error(char* msg)
 
 int main(int argc, char** argv)
 {
+	/* Declare variables to be used */
 	int prob_loss;
 	int prob_corruption;
 	int server_port_number;
 	char* server_hostname;
 	char* filename;
 
-	// Check to see if command line arguments are valid
+	/* Check to see if command line arguments are valid */
 	if(argc != 5)
 	{
 		perror("Usage: ./client server_hostname server_port_number ﬁlename prob_loss prob_corruption");
 	}
 
+	/* Initialize command line arguments */
 	server_hostname = argv[1];
     	server_port_number = atoi(argv[2]);
     	filename = argv[3];
 	prob_loss = atoi(argv[4]);
 	prob_corruption = atoi(argv[5]);
 	
+	/* Create a UDP socket */
+	int socket_fd;
+	socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    	
+	/* Check if socket was properly created */
+	if (socket_fd < 0) 
+        	error("ERROR opening socket");
+
+	/*Get the DNS information from the server */ 
+	struct hostent* server;
+	server = gethostbyname(server_hostname);	
+	if(server == NULL)
+		perror("Error: Not able to get server host by name");
+
 	return 0;
 }
